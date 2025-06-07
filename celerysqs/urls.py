c1f869a433d2bf9540ff14e6fa32f21da2714a7f
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2025-06-07 02:01:44 -0400

    Update the project with modern standards (#71)
    * switch over to uv + pyproject.toml
    * uv init to initialize project
    * uv add -r requirement.txt to add dependencies
    * uv add --dev -r requirements-dev.txt to add dev dependencies
    removes requirements[-dev].txt files
    
    * update depandabot.yml package-ecosystem to uv
    * run django-upgrade --target-version 4.2 on the project
    * Update pyproject.toml project description

--- celerysqs/urls.py
+++ celerysqs/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 
 from .views import TaskQueuer
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^queue/', TaskQueuer.as_view(), name='task_queuer'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^queue/', TaskQueuer.as_view(), name='task_queuer'),
 ]
